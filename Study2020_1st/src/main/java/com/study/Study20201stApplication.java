99c6884f0824fbaf5f220cf5a9f6f702877ce86a
smxlskan1500 <smxlskan1500@naver.com>
2020-04-17 00:32:11 +0900

    initial

---
+++ Study2020_1st/src/main/java/com/study/Study20201stApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study20201stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study20201stApplication.class, args);
+	}
+
+}
